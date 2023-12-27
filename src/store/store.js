3b857e1d610e9bf47b35822188f7df84ff56fe86
dhruv1145 <65786137+dhruv1145@users.noreply.github.com>
2023-12-27 15:59:31 +0530

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
+<<<<<<< HEAD
+import Slice from './slice.js';
+=======
 import Slice from './slice';
+>>>>>>> 079c4482ac64773f81ebb5553432f08df33b2b72
 
 const store = configureStore({
     reducer: {
